9324c75d22ac2dff4a65622815c2e99ffcae95cb
sebastian.cipolat <sebastiancipolat@gmail.com>
2023-10-27 09:40:31 -0300

    add new version

--- extract_videos.sh
+++ extract_videos.sh
@@ -1,52 +1,67 @@
 #!/bin/sh
-: '
----------------------------------------------
-                 _           _     _ 
-                | |         (_)   | |
-  __ _ _ __   __| |_ __ ___  _  __| |
- / _` | '_ \ / _` | '__/ _ \| |/ _` |
-| (_| | | | | (_| | | | (_) | | (_| |
- \__,_|_| |_|\__,_|_|  \___/|_|\__,_|
+# ---------------------------------------------
+#                  _           _     _
+#                 | |         (_)   | |
+#   __ _ _ __   __| |_ __ ___  _  __| |
+#  / _` | '_ \ / _` | '__/ _ \| |/ _` |
+# | (_| | | | | (_| | | | (_) | | (_| |
+#  \__,_|_| |_|\__,_|_|  \___/|_|\__,_|
+# 
+#  ___  ___ _ __ ___  ___ _ __
+# / __|/ __| '__/ _ \/ _ \ '_ \
+# \__ \ (__| | |  __/  __/ | | |
+# |___/\___|_|  \___|\___|_| |_|
+#
+#                            _           
+#                           | |          
+#  _ __ ___  ___ ___  _ __ __| | ___ _ __ 
+# | '__/ _ \/ __/ _ \| '__/ _` |/ _ \ '__|
+# | | |  __/ (_| (_) | | | (_| |  __/ |   
+# |_|  \___|\___\___/|_|  \__,_|\___|_|     
+#
+# --------------------------------------------
+# Created BY Sebastian Cipolat
+# @seba_cipolat
+# Argentina 2023
+# --------------------------------------------
+# Screen Recording Script
+# --------------------------------------------
+cat logo.txt
+echo ""
+file_counter="video_counter.txt"
+counter=$(head -n 1 $file_counter)
 
+if [ ! -d "extracted_videos" ]; then
+    mkdir "extracted_videos"
+fi
+if [  -d "video_list.tmp" ]; then
+    rm "video_list.tmp"
+fi
 
- ___  ___ _ __ ___  ___ _ __  
-/ __|/ __| '__/ _ \/ _ \ '_ \ 
-\__ \ (__| | |  __/  __/ | | |
-|___/\___|_|  \___|\___|_| |_|
-                                 
-                            _           
-                           | |          
- _ __ ___  ___ ___  _ __ __| | ___ _ __ 
-| '__/ _ \/ __/ _ \| '__/ _` |/ _ \ '__|
-| | |  __/ (_| (_) | | | (_| |  __/ |   
-|_|  \___|\___\___/|_|  \__,_|\___|_|     
+if [ -e "$file_counter" ]; then    
 
---------------------------------------------
-Created BY Sebastian Cipolat
-@seba_cipolat
-Argentina 2022  
---------------------------------------------
-Extraction Videos from android device Script
---------------------------------------------
-'
-#Stop screenrecord process
-echo "Kill screenrecord process.."
-pid=$(head -n 1 pid.tmp)
-kill  $pid
-wait $pid
-pkill -f "record.sh"
+    i=0
+    while [ "$i" -le "$counter" ]; do
+        fileName="rec.$i.mp4"
+        extracted_fileName="extracted_videos/$fileName"
+        echo "Start Extracting from device  /sdcard/screen_recorder/$fileName to $extracted_fileName"
+        adb pull /sdcard/screen_recorder/$fileName $extracted_fileName
+        echo "file '$fileName'" >> "extracted_videos/video_list.tmp"
+        i=$(( i + 1 ))
+    done 
 
-echo "Start Extracting Videos.."
+    cd "extracted_videos"
+    echo "Creating Final Video.."
+    ffmpeg -f concat -i video_list.tmp -c copy output.mp4
+    ls
 
-i=0
-counter=$(head -n 1 recorder.tmp)
+    if [  -e "output.mp4" ]; then
+        echo "-------------------------------------"
+        echo "... Final Video Created     OK    ..."
+        echo ".../extracted_videos/output.mp4  ..."
+        echo "-------------------------------------"
+    fi
 
-mkdir videos
-cd  videos
-
-while [ "$i" -le "$counter" ]; do
-    adb pull /sdcard/record_video_$i.mp4 record_video_$i.mp4
-    i=$(( i + 1 ))
-done 
-
-
+else
+ echo "El archivo $file_counter no encontrado :("
+fi
--- recorder.sh
+++ recorder.sh
@@ -1,48 +1,66 @@
 #!/bin/bash
-: '
----------------------------------------------
-                 _           _     _ 
-                | |         (_)   | |
-  __ _ _ __   __| |_ __ ___  _  __| |
- / _` | '_ \ / _` | '__/ _ \| |/ _` |
-| (_| | | | | (_| | | | (_) | | (_| |
- \__,_|_| |_|\__,_|_|  \___/|_|\__,_|
+# ---------------------------------------------
+#                  _           _     _
+#                 | |         (_)   | |
+#   __ _ _ __   __| |_ __ ___  _  __| |
+#  / _` | '_ \ / _` | '__/ _ \| |/ _` |
+# | (_| | | | | (_| | | | (_) | | (_| |
+#  \__,_|_| |_|\__,_|_|  \___/|_|\__,_|
+# 
+#  ___  ___ _ __ ___  ___ _ __
+# / __|/ __| '__/ _ \/ _ \ '_ \
+# \__ \ (__| | |  __/  __/ | | |
+# |___/\___|_|  \___|\___|_| |_|
+#
+#                            _           
+#                           | |          
+#  _ __ ___  ___ ___  _ __ __| | ___ _ __ 
+# | '__/ _ \/ __/ _ \| '__/ _` |/ _ \ '__|
+# | | |  __/ (_| (_) | | | (_| |  __/ |   
+# |_|  \___|\___\___/|_|  \__,_|\___|_|     
+#
+# --------------------------------------------
+# Created BY Sebastian Cipolat
+# @seba_cipolat
+# Argentina 2023
+# --------------------------------------------
+# Screen Recording Script
+# --------------------------------------------
+recorder(){
+    cat logo.txt
+    echo ""
+    if [ -e "video_counter.txt" ]; then
+        rm "video_counter.txt"
+    fi
+    
+    adb  shell rm -f *"/sdcard/screen_recorder/"*
+    adb  shell mkdir "/sdcard/screen_recorder/"
+  
+    # make sure SIGINT always works even in presence of infinite loops
+    close() {
+        trap - SIGINT SIGTERM SIGTERM # clear the trap
+        tput cnorm
+        adb  shell echo \04; wait
 
+        exit
+    }; trap exitScript SIGINT SIGTERM # set trap
 
- ___  ___ _ __ ___  ___ _ __  
-/ __|/ __| '__/ _ \/ _ \ '_ \ 
-\__ \ (__| | |  __/  __/ | | |
-|___/\___|_|  \___|\___|_| |_|
-                                 
-                            _           
-                           | |          
- _ __ ___  ___ ___  _ __ __| | ___ _ __ 
-| '__/ _ \/ __/ _ \| '__/ _` |/ _ \ '__|
-| | |  __/ (_| (_) | | | (_| |  __/ |   
-|_|  \___|\___\___/|_|  \__,_|\___|_|     
+    record(){   
+        printf "\n\n%*s\n\n" $((0)) "Use CTRL-C to stop the recording.."
+        videoCounter=0
 
---------------------------------------------
-Created BY Sebastian Cipolat
-@seba_cipolat
-Argentina 2022  
---------------------------------------------
-Screen Recordig Script
---------------------------------------------
-'
-counter=0
+        while true; do
+            fileName="rec.$videoCounter.mp4"
+            videoCounter=$((videoCounter + 1))
+            printf "\n%*s\n\n" $((0)) "Starting new recording: $fileName"                        
+            echo $videoCounter > video_counter.txt
+            adb  shell screenrecord /sdcard/screen_recorder/$fileName || adb shell echo \04
+            
+            wait;  continue
+        done
+   
+    }
+    record &&  close
+}
 
-while true
-do
-
-echo "Start Recording $counter"
-
-adb shell screenrecord /sdcard/record_video_$counter.mp4 & echo $$>pid.tmp
-wait $!
-
-counter=$((counter+1))
-
-echo $counter>recorder.tmp
-echo $process_id>pid.tmp
-
-done
-
+(recorder) && printf "\ncontinue main loop\n"; exit
